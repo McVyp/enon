aa7786ea009684721eb316c8d685542c5c067e50
McVyp <93302780+McVyp@users.noreply.github.com>
2023-02-07 23:00:44 +0545

    so far so good placeorder

--- pages/placeorder.js
+++ pages/placeorder.js
@@ -1,34 +1,84 @@
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
 import CheckoutWizard from '../components/CheckoutWizard';
 import Layout from '../components/Layout';
 import Link from 'next/link';
 import { Store } from '../utils/store';
+import Image from 'next/image';
 
-export default function PlaceorderScreen() {
-
-  const {state, dispatch} = useContext(Store);
-  const {cart} = state;
-  const {cartItems, shippingAddress, paymentMethod} = cart;
+export default function placeorderScreen() {
+    const {state, dispatch} = useContext(Store);
+    const {cart} = state;
+    const {cartItems, shippingAddress, paymentMethod} = cart;
   return (
-    <Layout title="Place Order">
-        <CheckoutWizard activeStep ={3}/>
+    <Layout title='Place Order'>
+        <CheckoutWizard activeStep={3}/>
         <h1 className='mb-4 text-xl'>Place Order</h1>
-        {cartItems.length === 0 ?(
+        {cartItems.length ===0?
+        (
             <div>
-                Cart is Empty. <Link href='/'>Go Shopping</Link>
+                Cart is empty. <Link href="/">Go Shopping</Link>
             </div>
         ):(
             <div className='grid md:grid-cols-4 md:gap-5'>
                 <div className='overflow-x-auto md:col-span-3'>
-                  <div className='card p-5'>
-                      <h2 className='mb-2 text-lg'>Shipping Address</h2>
-                      <div>
-                      {shippingAddress.fullName}, {shippingAddress.address}, {' '} {shippingAddress.city}, {shippingAddress.postalCode}, {''} {shippingAddress.country}
-                      </div>
-                      <div>
-                        <Link href='/shipping'>Edit</Link>
-                      </div>
-                  </div>
+                    <div className='card p-5'>
+                        <h2 className='mb-2 text-lg'>Shipping Address</h2>
+                        <div>
+                            {shippingAddress.fullName}, {shippingAddress.address}, {' '} {shippingAddress.city}, {shippingAddress.postalCode}. {' '} {shippingAddress.country}
+                        </div>
+                        <div>
+                            <Link href='/shipping'>Edit</Link>
+                        </div>
+                    </div>
+                    <div className='card p-5'>
+                        <h2 className='mb-2 text-lg'>Payment Method</h2>
+                        <div>{paymentMethod}</div>
+                        <div>
+                            <Link href='/payment'>Edit</Link>
+                        </div>
+                    </div>
+                    <div className='card p-5'>
+                        <h2 className='mb-2 text-lg'>Order Items</h2>
+                        <div>{paymentMethod}</div>
+                        <table className='min-w-full'>
+                            <thead className='border-b'>
+                                <tr>
+                                    <th className='px-5 text-left'>Item</th>
+                                    <th className='px-5 text-left'>Quantity</th>
+                                    <th className='px-5 text-left'>Price</th>
+                                    <th className='px-5 text-left'>Subtotal</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {cartItems.map((item)=> (
+                                    <tr key={item._id} className='border-b'>
+                                        <td>
+                                        <Link href={`/product/${item.slug}`}>
+                                            <a className='flex items-center'>
+                                                <Image
+                                                    src={item.image}
+                                                    alt={item.name}
+                                                    width={50}
+                                                    height={50}
+                                                />
+                                                &nbsp;
+                                                {item.name}
+                                            </a>
+                                        </Link>
+                                        </td>
+                                        <td className='p-5 text-right'>{item.quantity}</td>
+                                        <td className='p-5 text-right'>${item.price}</td>
+                                        <td className='p-5 text-right'>
+                                            ${item.quantity * item.price}
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                        <div>
+                            <Link href='/cart'>Edit</Link>
+                        </div>
+                    </div>
                 </div>
             </div>
         )}
